96017b0b5ad7d612bef0035f337f7f37647cff4f
vishesh280204 <130971127+vishesh280204@users.noreply.github.com>
2026-07-07 23:41:34 +0530

    Created basic express server

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -3,13 +3,14 @@ import "dotenv/config";
 
 const connectDB = async () => {
   try {
-    console.log("URI:", process.env.MONGODB_URI);
+    
 
     await mongoose.connect(process.env.MONGODB_URI!);
 
     console.log("✅ Database connected");
   } catch (err) {
     console.error(err);
+    process.exit(1)
   }
 };
 
---
+++ server/src/utils/ApiResponse.ts
@@ -0,0 +1,17 @@
+export class ApiResponse {
+    statusCode:number
+    message:string
+    data:any
+    success:boolean
+    constructor(
+        statusCode:number,
+        message="success",
+        data:any,
+        success:boolean
+    ){
+        this.statusCode=statusCode
+        this.message=message
+        this.data=data
+        this.success=success
+    }
+}
