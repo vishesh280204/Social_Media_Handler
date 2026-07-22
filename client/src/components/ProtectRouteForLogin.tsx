98dd13827a61ff2e764b6224089d5de32a46836b
vishesh280204 <130971127+vishesh280204@users.noreply.github.com>
2026-07-23 00:19:50 +0530

    fix: update ProtectRouteForLogin to render Outlet instead of Navigate for unauthenticated users

--- client/src/components/ProtectRouteForLogin.tsx
+++ client/src/components/ProtectRouteForLogin.tsx
@@ -1,11 +1,11 @@
 import { useAuthContext } from '../context/authContext'
-import { Navigate } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 
 const ProtectRouteForLogin = () => {
     const {isAuthenticated}=useAuthContext()
     if(isAuthenticated) return (<Navigate to={"/dashboard"}/>)
   return (
-     <Navigate to={"/login"}/>
+     <Outlet/>
   )
 }
 
